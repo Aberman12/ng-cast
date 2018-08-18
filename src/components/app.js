0bff591f93c4bb79f1941f291b750dad781c3155
aberman12 <aberman12@yahoo.com>
2018-08-17 17:29:22 -0700

    done

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,24 @@
 angular.module('video-player')
-.controller('app-controller', function(){
-    this.exampleData = window.exampleVideoData;
-    this.currentVideo = this.exampleData[0];
+.controller('app-controller', function(youTube){
+    youTube.search(this.query, (data) => {
+        this.exampleData = data;
+        this.currentVideo = data[0];
+    });
+    this.query = 'asdf';
+    this.changeQuery = (query) => {
+        console.log('query app', query);
+        this.query = query;
+        youTube.search(this.query, (data) => {
+            this.exampleData = data;
+            this.currentVideo = data[0];
+        });
+    }
     this.changeVid = (video) => {
         console.log('tesetset', video)
         this.currentVideo = this.exampleData[video];
     }
     console.log('currentVideo on app.js', this.currentVideo)
+
 })
 .component('app', {
   controller: 'app-controller',
--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,17 @@
 angular.module('video-player')
 
 .component('search', {
+  bindings: {
+    query: '<'
+  },
+  controller: function(){
+
+    this.query2 = '';
+    this.func = () =>{
+      // query = this.query2;
+      console.log('query2', this.query2)
+    }
+
+  },
   templateUrl: 'src/templates/search.html'
 });
